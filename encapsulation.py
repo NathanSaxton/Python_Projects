4f6b7909bb9bbd6595842f8e32156c22bb5e8897
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-03-14 11:07:05 -0600

    Update encapsulation.py
    added comments

--- encapsulation.py
+++ encapsulation.py
@@ -4,7 +4,7 @@ class protected:
     def __init__(self):
         self._protectedVar = 0
 
-class private:
+class private: 
     def __init__(self):
         self.__privateVar = 0
     def getPrivate(self):
@@ -12,9 +12,10 @@ class private:
     def setPrivate(self, value):
         self.__privateVar = value        
 
-obj1 = protected()
-obj1._protectedVar = 7
-obj2 = private()
-obj2.setPrivate(2002)
+obj1 = protected()#instantiates the protected class
+obj1._protectedVar = 7#changes the value of the protected class
+obj2 = private()#instantiates the private class
+obj2.setPrivate(2002)#changes the varible within private
+#access both changed values in the following print statement
 print("My daughter is {} years old today, she was born in {}.".format(obj1._protectedVar,obj2.getPrivate()))
 
