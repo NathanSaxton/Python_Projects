f1e26af7a08f9d824d7cf41e898ef15d73334575
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-03-14 03:22:28 -0600

    Update phonebook_gui.py

--- Phonebook_Project/phonebook_gui.py
+++ Phonebook_Project/phonebook_gui.py
@@ -13,17 +13,17 @@ def load_gui(self):
     self.lbl_phone.grid(row=4,column=0,padx=(27,0),pady=(10,0),sticky=N+W)
     self.lbl_email = tk.Label(self.master,text="Email: ")
     self.lbl_email.grid(row=6,column=0,padx=(27,0),pady=(10,0),sticky=N+W)
-    self.lbl_user = tk.Label(self.master,text="Email: ")
+    self.lbl_user = tk.Label(self.master,text="User: ")
     self.lbl_user.grid(row=0,column=2,padx=(0,0),pady=(10,0),sticky=N+W)
 
     self.txt_fname = tk.Entry(self.master,text="")
     self.txt_fname.grid(row=1,column=0,rowspan=1,columnspan=2,padx=(30,40),pady=(0,0),sticky=N+E+W)
     self.txt_lname = tk.Entry(self.master,text="")
-    self.txt_lname.grid(row=1,column=0,rowspan=1,columnspan=2,padx=(30,40),pady=(0,0),sticky=N+E+W)
+    self.txt_lname.grid(row=3,column=0,rowspan=1,columnspan=2,padx=(30,40),pady=(0,0),sticky=N+E+W)
     self.txt_phone = tk.Entry(self.master,text="")
-    self.txt_phone.grid(row=1,column=0,rowspan=1,columnspan=2,padx=(30,40),pady=(0,0),sticky=N+E+W)
+    self.txt_phone.grid(row=5,column=0,rowspan=1,columnspan=2,padx=(30,40),pady=(0,0),sticky=N+E+W)
     self.txt_email = tk.Entry(self.master,text="")
-    self.txt_email.grid(row=1,column=0,rowspan=1,columnspan=2,padx=(30,40),pady=(0,0),sticky=N+E+W)
+    self.txt_email.grid(row=7,column=0,rowspan=1,columnspan=2,padx=(30,40),pady=(0,0),sticky=N+E+W)
 
     #listbox with a scrollbar
     self.scrollbar1 = Scrollbar(self.master,orient=VERTICAL)
@@ -35,11 +35,11 @@ def load_gui(self):
 
     self.btn_add = tk.Button(self.master,width=12,height=2,text="Add",command=lambda: phonebook_func.addToList(self))
     self.btn_add.grid(row=8,column=0,padx=(25,0),pady=(45,10),sticky=W)
-    self.btn_update = tk.Button(self.master,width=12,height=2,text="Update",command=lambda: phonebook_func.addToList(self))
+    self.btn_update = tk.Button(self.master,width=12,height=2,text="Update",command=lambda: phonebook_func.onUpdate(self))
     self.btn_update.grid(row=8,column=1,padx=(15,0),pady=(45,10),sticky=W)
-    self.btn_delete = tk.Button(self.master,width=12,height=2,text="Delete",command=lambda: phonebook_func.addToList(self))
+    self.btn_delete = tk.Button(self.master,width=12,height=2,text="Delete",command=lambda: phonebook_func.onDelete(self))
     self.btn_delete.grid(row=8,column=2,padx=(15,0),pady=(45,10),sticky=W)
-    self.btn_close = tk.Button(self.master,width=12,height=2,text="Close",command=lambda: phonebook_func.addToList(self))
+    self.btn_close = tk.Button(self.master,width=12,height=2,text="Close",command=lambda: phonebook_func.ask_quit(self))
     self.btn_close.grid(row=8,column=4,columnspan=1,padx=(15,0),pady=(45,10),sticky=E)
 
     phonebook_func.create_db(self)
