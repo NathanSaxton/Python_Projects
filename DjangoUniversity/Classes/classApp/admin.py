8c261dc49a834621e2e064c9586fe9f1f02a22f0
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-03-29 14:13:20 -0600

    Added comments

--- DjangoUniversity/Classes/classApp/admin.py
+++ DjangoUniversity/Classes/classApp/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from .models import DjangoClasses
 
-admin.site.register(DjangoClasses)
+admin.site.register(DjangoClasses)#registering the DjangoClasses class
--- DjangoUniversity/Classes/classApp/models.py
+++ DjangoUniversity/Classes/classApp/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 # Create your models here.
-class DjangoClasses(models.Model):
+class DjangoClasses(models.Model):#setting up DjangoClasses and it's attributes
     title = models.CharField(max_length=60, default="", blank=True, null=False)
     courseNumber = models.IntegerField(blank=False, null=False)
     instructor = models.CharField(max_length=25, default="", blank=True, null=False)
     duration = models.FloatField(max_length=6, blank=True, null=False)
 
-    objects = models.Manager()
+    objects = models.Manager()#assign the manager a variable
 
     def __str__(self):
-        return self.title
+        return self.instructor #use the instructor of the class to identify.
